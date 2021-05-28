8096b19290ed569d2f3cba08d0d9f6b8cced5720
hshar94 <hemantsharma@Hemants-MacBook-Air.local>
2021-05-28 12:53:29 +0530

    new changes

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 custombucket = "destinationtxt"
 table = "intellipaat"
-databasehost = "database-1.cyro8arsnhbc.us-east-2.rds.amazonaws.com"
+databasehost = "database-1.c14bnmt8jlz0.us-east-1.rds.amazonaws.com"
 duser = "intel"
 dpass = "intel123"
 s3database = "intellipaat"
